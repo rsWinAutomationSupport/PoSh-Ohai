fb2435703ab76465098c50fbf2e7c9fed22f685e
unknown <csab7155@VCSABA1.RACKSPACE.CORP>
2014-04-08 16:29:41 +0100

    memory.ps1 collects memory usage information

---
+++ plugins/memory.ps1
@@ -0,0 +1,22 @@
+﻿$provides = "memory"
+
+function Collect-Data {
+    $meminfo = [ordered]@{}
+	
+	try
+	{
+		$memory = Get-WmiObject Win32_PerfRawData_PerfOS_Memory
+		
+		$meminfo["AvailableMBytes"] = $memory.AvailableMBytes
+		$meminfo["CommittedMBytes"] = [int]($memory.CommittedBytes / (1024*1024))
+		$meminfo["SystemCacheMBytes"] = [int]($memory.CacheBytes / (1024*1024))
+		$meminfo["NonPagedPoolMBytes"] = [int]($memory.PoolNonpagedBytes / (1024*1024))
+		$meminfo["PagedPoolMBytes"] = [int]($memory.PoolPagedBytes / (1024*1024))
+	}
+	catch
+	{
+		$meminfo["Error"] = $_.Exception.ToString()
+	}
+	
+    @{ "memory" = $meminfo}
+}
