88581682ce75f68e55ddcd8f6681687524483daf
Hounsou Dansou <hounsou.dansou@rackspace.com>
2014-10-21 15:35:12 -0500

    Added the service plugin

---
+++ plugins/services.ps1
@@ -0,0 +1,8 @@
+﻿$provides = "services"
+
+function Collect-Data {
+  $services = [ordered]@{}
+  $list_services = Get-WmiObject -Class Win32_Service | select Name,ProcessId,State,StartMode,Status
+
+  [ordered]@{"services" = $list_services}
+}
