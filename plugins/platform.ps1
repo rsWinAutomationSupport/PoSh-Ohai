9b6ec67aa043f15ef631eba879584e8de206972d
Hounsou Dansou <hounsou.dansou@rackspace.com>
2015-01-22 18:47:04 -0600

    Corrected OS detection

--- plugins/platform.ps1
+++ plugins/platform.ps1
@@ -1,10 +1,13 @@
 ﻿$provides = 'os','platform_family','platform','platform_version','platform_architecture','platform_servicepack_major','platform_servicepack_minor','powershell_version'
-function Collect-Data {    
-    $os = @{ 
-        'os' = $(((Get-CimInstance Win32_OperatingSystem).RegisteredUser).split(' ')[0])
+function Collect-Data {
+    if(((Get-CimInstance Win32_OperatingSystem).CreationClassName) -eq 'Win32_OperatingSystem'){
+        $OperatingSystem = 'Windows'
     }
-    $platform_family    = @{ 
-        'platform_family' = $(((Get-CimInstance Win32_OperatingSystem).RegisteredUser).split(' ')[0])
+    $os = @{
+        'os' = $OperatingSystem
+    }
+    $platform_family    = @{
+        'platform_family' = $OperatingSystem
     }
     $platform    = @{
         'platform' = $((Get-CimInstance Win32_OperatingSystem).Caption)
@@ -13,25 +16,23 @@ function Collect-Data {
         'platform_version' = $((Get-CimInstance Win32_OperatingSystem).version)
     }
     $platform_architecture    = @{
-        'platform_architecture' = $((Get-CimInstance Win32_OperatingSystem).OSArchitecture) 
+        'platform_architecture' = $((Get-CimInstance Win32_OperatingSystem).OSArchitecture)
     }
     $platform_servicepack_major    = @{
-        'platform_servicepack_major' = $((Get-CimInstance Win32_OperatingSystem).ServicePackMajorVersion) 
+        'platform_servicepack_major' = $((Get-CimInstance Win32_OperatingSystem).ServicePackMajorVersion)
     }
     $platform_servicepack_minor    = @{
-        'platform_servicepack_minor' = $((Get-CimInstance Win32_OperatingSystem).ServicePackMinorVersion) 
+        'platform_servicepack_minor' = $((Get-CimInstance Win32_OperatingSystem).ServicePackMinorVersion)
     }
     $powershell_version    = @{
-        'powershell_version' = $($PSVersionTable.PSVersion.ToString()) 
+        'powershell_version' = $($PSVersionTable.PSVersion.ToString())
     }
-    $os + 
-    $platform_family + 
-    $platform + 
-    $platform_version + 
-    $platform_architecture + 
+    $os +
+    $platform_family +
+    $platform +
+    $platform_version +
+    $platform_architecture +
     $platform_servicepack_major +
-    $platform_servicepack_minor + 
+    $platform_servicepack_minor +
     $powershell_version
 }
-
-Collect-Data
