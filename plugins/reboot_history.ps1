972f84f864fb4bbeceacd988b0e51cba1f89f3dc
Hounsou Dansou <hounsou.dansou@rackspace.com>
2015-02-16 13:18:37 -0600

    Made reboot_history plugin powershell v2 compatible. A partial work of #3

--- plugins/reboot_history.ps1
+++ plugins/reboot_history.ps1
@@ -1,18 +1,22 @@
-﻿$provides = "reboot_history"
+﻿$provides = 'reboot_history'
 
 function Collect-Data {
-  $reboot_history = [ordered]@{}
-  $event_1074 = Get-WinEvent -FilterHashtable @{logname='System'; id=1074}  |
-  ForEach-Object {
-    $rv = New-Object PSObject | Select-Object Date, User, Action, Process, Reason, ReasonCode, Comment
-    $rv.Date = $_.TimeCreated
-    $rv.User = $_.Properties[6].Value
-    $rv.Process = $_.Properties[0].Value
-    $rv.Action = $_.Properties[4].Value
-    $rv.Reason = $_.Properties[2].Value
-    $rv.ReasonCode = $_.Properties[3].Value
-    $rv.Comment = $_.Properties[5].Value
-    $rv
-  } | Select-Object Date, Action, Reason, User
-  [ordered]@{"reboot_history"=$event_1074} 
+    $output = New-Object System.Collections.Specialized.OrderedDictionary
+    $reboot_history = New-Object System.Collections.Specialized.OrderedDictionary
+    
+    $event_1074 = Get-WinEvent -FilterHashtable @{logname='System'; id=1074}  |
+        ForEach-Object {
+            $rv = New-Object PSObject | Select-Object Date, User, Action, Process, Reason, ReasonCode, Comment
+            $rv.Date = $_.TimeCreated
+            $rv.User = $_.Properties[6].Value
+            $rv.Process = $_.Properties[0].Value
+            $rv.Action = $_.Properties[4].Value
+            $rv.Reason = $_.Properties[2].Value
+            $rv.ReasonCode = $_.Properties[3].Value
+            $rv.Comment = $_.Properties[5].Value
+            $rv
+        } | Select-Object Date, Action, Reason, User
+    
+    $output.Add('reboot_history' , $event_1074)
+    $output 
 }
