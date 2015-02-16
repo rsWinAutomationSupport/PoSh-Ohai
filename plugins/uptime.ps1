d9f4ca91fa906d38d588a20ad56a889cce5a4884
Hounsou Dansou <hounsou.dansou@rackspace.com>
2015-02-16 13:24:19 -0600

    Made uptime plugin powershell v2 compatible. A partial work of #3

--- plugins/uptime.ps1
+++ plugins/uptime.ps1
@@ -1,9 +1,10 @@
-﻿$provides = "uptime", "uptime_seconds"
+﻿$provides = 'uptime', 'uptime_seconds'
+
 function Collect-Data {
     $uptimeSeconds = (Get-WmiObject Win32_PerfFormattedData_PerfOS_System).SystemUpTime
     $TimeSpan = New-TimeSpan -Seconds $uptimeSeconds
     $uptimeString = '{0} days {1} hours {2} minutes {3} seconds' -f $TimeSpan.Days,$TimeSpan.Hours,$TimeSpan.Minutes,$TimeSpan.Seconds
-    $uptime = @{ "uptime" = $uptimeString}
-    $uptime_seconds = @{"uptime_seconds" = $uptimeSeconds}
+    $uptime = @{ 'uptime' = $uptimeString}
+    $uptime_seconds = @{'uptime_seconds' = $uptimeSeconds}
     $uptime_seconds + $uptime
 }
